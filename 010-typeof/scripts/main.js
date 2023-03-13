070326c80546f4b863ba9aac8207496aba317788
Karla <kclemms@hotmail.com>
2023-03-12 21:23:36 -0700

    typeof

---
+++ 010-typeof/scripts/main.js
@@ -0,0 +1,15 @@
+let dynamicValue = 50 //dynamicValue is a number
+console.log("dynamicValue is 50 which is typeof ", (typeof dynamicValue))
+
+dynamicValue = "bar" //string
+console.log("dynamicValue is bar which is typeof ", (typeof dynamicValue))
+
+dynamicValue = true //boolean
+console.log("dynamicValue is true which is typeof ", (typeof dynamicValue))
+
+dynamicValue = 50 //dynamicValue is a number
+const result = dynamicValue + 1 //now is a string
+console.log("result of 50 + 1 is type of - ", (typeof result)) // 51
+
+
+
